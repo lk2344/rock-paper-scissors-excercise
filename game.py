9f0403788255ecb933f208c2995992f359f34941
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 14:36:38 -0400

    computer choice

--- game.py
+++ game.py
@@ -16,6 +16,11 @@ print("You chose:", user_input)
 
 #simulate computer selection
 
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("Computer chose:", computer_choice)
+
+
 #determine winner
 
 
