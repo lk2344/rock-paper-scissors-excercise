e014f5719e19ee9b4e5b9a5b4ba887b4c28c38a2
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 15:05:34 -0400

    fix indentations

--- game.py
+++ game.py
@@ -19,33 +19,33 @@ valid_options = ["rock", "paper", "scissors"]
 
 		#simulate computer selection
 
-		computer_choice = random.choice(valid_options)
-		print("Computer chose:", computer_choice)
+			computer_choice = random.choice(valid_options)
+			print("Computer chose:", computer_choice)
 
 
-		#determine winner
+			#determine winner
 
-		if user_input == "rock":
-			if computer_choice == "rock":
-				print("TIE")
-			elif computer_choice == "paper":
-				print("You Lost :(")
-			elif computer_choice == "scissors":
-				print("You Win!")
-		elif user_input == "paper":
-			if computer_choice == "paper":
-				print("TIE")
-			elif computer_choice == "scissors":
-				print("You Lost :(")
-			elif computer_choice == "rock":
-				print("You Win!")
-		elif user_input == "scissors":
-			if computer_choice == "scissors":
-				print("TIE")
-			elif computer_choice == "rock":
-				print("You Lost :(")
-			elif computer_choice == "paper"
-				print("You Win!")										
+				if user_input == "rock":
+					if computer_choice == "rock":
+						print("TIE")
+					elif computer_choice == "paper":
+						print("You Lost :(")
+					elif computer_choice == "scissors":
+						print("You Win!")
+				elif user_input == "paper":
+					if computer_choice == "paper":
+						print("TIE")
+					elif computer_choice == "scissors":
+						print("You Lost :(")
+					elif computer_choice == "rock":
+						print("You Win!")
+				elif user_input == "scissors":
+					if computer_choice == "scissors":
+						print("TIE")
+					elif computer_choice == "rock":
+						print("You Lost :(")
+					elif computer_choice == "paper"
+						print("You Win!")										
 
 
 	if user_input not in valid_options:
