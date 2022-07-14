10fcbed510f0e974e627fdb760ca6a00c50fd4c2
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 16:06:45 -0400

    final edit

--- game.py
+++ game.py
@@ -1,53 +1,43 @@
-game.py
+
 #this is the "game.py" file...
 
 import random
 
 print("Welcome to my Rock, Paper, Scissors Game")
 
-
 #process user inputs
-
-user_input = input("Please choose one ('rock', 'paper', 'scisors'")
+user_input = input("Please choose one 'rock', 'paper', 'scisors'")
 user_input = user_input.lower()
 
-
 #validate user input
-
 valid_options = ["rock", "paper", "scissors"]
-	if user_input in valid_options:
-		print("You chose:", user_input)
+if user_input in valid_options:
+	print("You chose:", user_input)
 
 
-		#simulate computer selection
+	#simulate computer selection
+	computer_choice = random.choice(valid_options)
+		print("Computer chose:", computer_choice)
 
-		computer_choice = random.choice(valid_options)
-			print("Computer chose:", computer_choice)
 
-
-			#determine winner
-
-			if user_input == "rock":
-				if computer_choice == "rock":
-					print("TIE")
-				elif computer_choice == "paper":
-					print("You Lost :(")
-				elif computer_choice == "scissors":
-					print("You Win!")
-			elif user_input == "paper":
-				if computer_choice == "paper":
-					print("TIE")
-				elif computer_choice == "scissors":
-					print("You Lost :(")
-				elif computer_choice == "rock":
-					print("You Win!")
-			elif user_input == "scissors":
-				if computer_choice == "scissors":
-					print("TIE")
-				elif computer_choice == "rock":
-					print("You Lost :(")
-				elif computer_choice == "paper"
-					print("You Win!")										
+	#determine winner
+	if user_input == computer_choice:
+		print("TIE")	
+	elif user_input == "rock":
+		if computer_choice == "scissors":
+			print("You Win!")
+		else:
+			print("You Lost :(")
+	elif user_input == "paper":
+		if computer_choice == "rock":
+			print("You Win!")
+		else:
+			print("You Lost :(")
+	elif user_input == "scissors":
+		if computer_choice == "paper":
+			print("You Win!")
+		else:
+			print("You Lost :(")								
 
 
 	if user_input not in valid_options:
