56e09c1a2ba27df97225d60baf325b21b28c51f7
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 14:48:04 -0400

    determine winner

--- game.py
+++ game.py
@@ -23,6 +23,28 @@ print("Computer chose:", computer_choice)
 
 #determine winner
 
+if user_input == "rock":
+	if computer_choice == "rock":
+		print("TIE")
+	elif computer_choice == "paper":
+		print("You Lost :(")
+	elif computer_choice == "scissors":
+		print("You Win!")
+elif user_input == "paper":
+	if computer_choice == "paper":
+		print("TIE")
+	elif computer_choice == "scissors":
+		print("You Lost :(")
+	elif computer_choice == "rock":
+		print("You Win!")
+elif user_input == "scissors":
+	if computer_choice == "scissors":
+		print("TIE")
+	elif computer_choice == "rock":
+		print("You Lost :(")
+	elif computer_choice == "paper"
+		print("You Win!")										
+
 
 #display result
 
