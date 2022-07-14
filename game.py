24816058a41fc8f22ecbce106a9628aa9dac7865
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 14:28:20 -0400

    user input

--- game.py
+++ game.py
@@ -1,4 +1,24 @@
 game.py
 #this is the "game.py" file...
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Welcome to my Rock, Paper, Scissors Game")
+
+
+#process user inputs
+
+user_input = input("Please choose one ('rock', 'paper', 'scisors'")
+
+print("You chose:", user_input)
+
+
+#Validate user inputs
+
+
+#simulate computer selection
+
+#determine winner
+
+
+#display result
+
+
