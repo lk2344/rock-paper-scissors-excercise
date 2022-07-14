b2141bf714c246b1aa5e98eaa80e4236624528c4
lk2344 <108551638+lk2344@users.noreply.github.com>
2022-07-14 14:58:39 -0400

    validate options

--- game.py
+++ game.py
@@ -7,45 +7,48 @@ print("Welcome to my Rock, Paper, Scissors Game")
 #process user inputs
 
 user_input = input("Please choose one ('rock', 'paper', 'scisors'")
-
-print("You chose:", user_input)
-
-
-#Validate user inputs
+user_input = user_input.lower()
 
 
-#simulate computer selection
+#validate user input
 
 valid_options = ["rock", "paper", "scissors"]
-computer_choice = random.choice(valid_options)
-print("Computer chose:", computer_choice)
+	if user_input in valid_options:
+		print("You chose:", user_input)
 
 
-#determine winner
+		#simulate computer selection
 
-if user_input == "rock":
-	if computer_choice == "rock":
-		print("TIE")
-	elif computer_choice == "paper":
-		print("You Lost :(")
-	elif computer_choice == "scissors":
-		print("You Win!")
-elif user_input == "paper":
-	if computer_choice == "paper":
-		print("TIE")
-	elif computer_choice == "scissors":
-		print("You Lost :(")
-	elif computer_choice == "rock":
-		print("You Win!")
-elif user_input == "scissors":
-	if computer_choice == "scissors":
-		print("TIE")
-	elif computer_choice == "rock":
-		print("You Lost :(")
-	elif computer_choice == "paper"
-		print("You Win!")										
+		computer_choice = random.choice(valid_options)
+		print("Computer chose:", computer_choice)
 
 
-#display result
+		#determine winner
 
+		if user_input == "rock":
+			if computer_choice == "rock":
+				print("TIE")
+			elif computer_choice == "paper":
+				print("You Lost :(")
+			elif computer_choice == "scissors":
+				print("You Win!")
+		elif user_input == "paper":
+			if computer_choice == "paper":
+				print("TIE")
+			elif computer_choice == "scissors":
+				print("You Lost :(")
+			elif computer_choice == "rock":
+				print("You Win!")
+		elif user_input == "scissors":
+			if computer_choice == "scissors":
+				print("TIE")
+			elif computer_choice == "rock":
+				print("You Lost :(")
+			elif computer_choice == "paper"
+				print("You Win!")										
+
+
+	if user_input not in valid_options:
+		print("INVALID CHOICE. TRY AGAIN")			
+		exit()
 
